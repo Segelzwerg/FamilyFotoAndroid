87eb39ccf174bce78c4122c80e9a11b6f2b4d899
Segelzwerg <marcel.haas@hhu.de>
2020-09-28 22:19:12 +0200

    ADD: test for check state

---
+++ app/src/test/java/com/segelzwerg/familyfotoandroid/TestUsernameWatcher.java
@@ -0,0 +1,33 @@
+package com.segelzwerg.familyfotoandroid;
+
+import android.text.Editable;
+
+import com.segelzwerg.familyfotoandroid.ui.UsernameWatcher;
+import com.segelzwerg.familyfotoandroid.ui.elements.LoginButton;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class TestUsernameWatcher {
+
+    private LoginButton loginButton;
+
+    @BeforeEach
+    public void setUp() {
+        loginButton = mock(LoginButton.class);
+    }
+
+    @Test
+    public void testAfterTextChanged() {
+        Editable editable = mock(Editable.class);
+        when(editable.toString()).thenReturn("abc");
+        UsernameWatcher usernameWatcher = new UsernameWatcher(loginButton);
+        usernameWatcher.afterTextChanged(editable);
+        verify(loginButton, times(1)).checkState();
+    }
+}
