b909c1063aa1aad82b2ea4886ae0cc741708075d
Segelzwerg <marcel.haas@hhu.de>
2020-09-29 11:57:17 +0200

    ADD: main activity

---
+++ app/src/main/java/com/segelzwerg/familyfotoandroid/ui/MainActivity.java
@@ -0,0 +1,36 @@
+package com.segelzwerg.familyfotoandroid.ui;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.segelzwerg.familyfotoandroid.familiyfotoservice.LoginCredentials;
+import com.segelzwerg.familyfotoandroid.familiyfotoservice.UserManager;
+
+/**
+ * The main activity of this app.
+ */
+public class MainActivity extends AppCompatActivity {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView welcomeTextView = new TextView(this);
+
+        AccountManager accountManager = AccountManager.get(this);
+        UserManager userManager = new UserManager(accountManager);
+        userManager.saveAccount(new LoginCredentials("marcel", "1234"));
+        Account[] accounts = accountManager.getAccounts();
+
+        String welcomeText = String.format("Welcome %s to Family Foto!", accounts[0].name);
+        welcomeTextView.setText(welcomeText);
+        setContentView(welcomeTextView);
+    }
+}
