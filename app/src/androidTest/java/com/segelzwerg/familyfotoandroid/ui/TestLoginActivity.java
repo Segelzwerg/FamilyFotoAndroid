ec53de16a353de38370bac10f8d5a6e2b8e32bec
Segelzwerg <marcel.haas@hhu.de>
2020-09-29 12:18:38 +0200

    ADD: test for login

--- app/src/androidTest/java/com/segelzwerg/familyfotoandroid/ui/TestLoginActivity.java
+++ app/src/androidTest/java/com/segelzwerg/familyfotoandroid/ui/TestLoginActivity.java
@@ -1,9 +1,10 @@
 package com.segelzwerg.familyfotoandroid.ui;
 
-import androidx.test.core.app.ActivityScenario;
-
 import com.segelzwerg.familyfotoandroid.R;
+import com.segelzwerg.familyfotoandroid.familiyfotoservice.FamilyFotoServerService;
+import com.segelzwerg.familyfotoandroid.familiyfotoservice.LoginCredentials;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
@@ -16,18 +17,36 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static de.mannodermaus.junit5.ActivityScenarioExtension.launch;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class TestLoginActivity {
     @RegisterExtension
     public ActivityScenarioExtension<LoginActivity> scenarioExtension = launch(LoginActivity.class);
 
+    private FamilyFotoServerService familyFotoServerService;
+
+    @BeforeEach
+    public void setUp() {
+        familyFotoServerService = mock(FamilyFotoServerService.class);
+    }
+
     @Test
     public void testButtonEnabled() {
-        ActivityScenario<LoginActivity> scenario = scenarioExtension.getScenario();
         onView(withId(R.id.username)).perform(typeText("marcel"),
                 closeSoftKeyboard());
         onView(withId(R.id.password)).perform(typeText("12345678"),
                 closeSoftKeyboard());
         onView(withId(R.id.login)).check(matches(isEnabled()));
+    }
+
+    @Test
+    public void testLoginSuccess() {
+        onView(withId(R.id.username)).perform(typeText("marcel"),
+                closeSoftKeyboard());
+        onView(withId(R.id.password)).perform(typeText("12345678"),
+                closeSoftKeyboard());
+        verify(familyFotoServerService).login(any(LoginCredentials.class));
     }
 }
