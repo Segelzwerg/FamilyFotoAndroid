8dd884f8f6210d8e6a1f19b2b8e5a6e932250a84
Segelzwerg <marcel.haas@hhu.de>
2020-09-30 12:53:07 +0200

    ADD: module for base url during testing

---
+++ app/src/main/java/com/segelzwerg/familyfotoandroid/familiyfotoservice/BaseUrlModule.java
@@ -0,0 +1,18 @@
+package com.segelzwerg.familyfotoandroid.familiyfotoservice;
+
+import dagger.Module;
+import dagger.Provides;
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.components.ApplicationComponent;
+import okhttp3.HttpUrl;
+
+@Module
+@InstallIn(ApplicationComponent.class)
+public class BaseUrlModule {
+    public static final int MOCK_SERVER_PORT = 5000;
+
+    @Provides
+    public static HttpUrl provideBaseUrl() {
+        return HttpUrl.parse("http://localhost:" + MOCK_SERVER_PORT);
+    }
+}
