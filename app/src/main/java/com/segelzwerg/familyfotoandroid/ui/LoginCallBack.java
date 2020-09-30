53097fb5ff1739297f2d7f9909488571f3383caa
Segelzwerg <marcel.haas@hhu.de>
2020-09-30 13:00:45 +0200

    ADD: login call back

---
+++ app/src/main/java/com/segelzwerg/familyfotoandroid/ui/LoginCallBack.java
@@ -0,0 +1,39 @@
+package com.segelzwerg.familyfotoandroid.ui;
+
+import android.content.Context;
+import android.content.Intent;
+
+import lombok.SneakyThrows;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static androidx.core.content.ContextCompat.startActivity;
+
+class LoginCallBack<AuthToken> implements Callback<AuthToken> {
+    private final Context context;
+
+    public LoginCallBack(Context context) {
+        this.context = context;
+    }
+
+    /**
+     * Redirects to MainActivity.
+     * {@inheritDoc}
+     */
+    @Override
+    public void onResponse(Call<AuthToken> call, Response<AuthToken> response) {
+        Intent intent = new Intent(context, MainActivity.class);
+        startActivity(context, intent, null);
+    }
+
+    /**
+     * {@inheritDoc}
+     **/
+    @SneakyThrows
+    @Override
+    public void onFailure(Call<AuthToken> call, Throwable t) {
+        throw t;
+    }
+
+}
