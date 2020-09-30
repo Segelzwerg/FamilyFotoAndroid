862e162fa6e5083ac4ec2f95e68e9e107da49403
Segelzwerg <marcel.haas@hhu.de>
2020-09-30 13:02:25 +0200

    ADD: hilt app

---
+++ app/src/main/java/com/segelzwerg/familyfotoandroid/FamilyFotoAndroidApplication.java
@@ -0,0 +1,16 @@
+package com.segelzwerg.familyfotoandroid;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class FamilyFotoAndroidApplication extends Application {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
