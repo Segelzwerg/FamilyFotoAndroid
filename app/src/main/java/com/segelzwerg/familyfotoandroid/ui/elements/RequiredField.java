aee587a52f2877a0dcc5376e7fb423d286754109
Segelzwerg <marcel.haas@hhu.de>
2020-10-03 18:19:38 +0200

    REFACTOR: string extraction

--- app/src/main/java/com/segelzwerg/familyfotoandroid/ui/elements/RequiredField.java
+++ app/src/main/java/com/segelzwerg/familyfotoandroid/ui/elements/RequiredField.java
@@ -1,6 +1,7 @@
 package com.segelzwerg.familyfotoandroid.ui.elements;
 
 import android.content.Context;
+import android.text.Editable;
 import android.util.AttributeSet;
 
 import androidx.annotation.NonNull;
@@ -46,6 +47,15 @@ public class RequiredField extends AppCompatEditText {
      * @return input as string
      */
     public String getString() {
-        return Objects.requireNonNull(getText()).toString();
+        return extractString(Objects.requireNonNull(getText()));
+    }
+
+    /**
+     * Extracts the editable content as string.
+     * @param editable from which the strong should be extracted
+     * @return the content as string
+     */
+    private String extractString(Editable editable) {
+        return editable.toString();
     }
 }
