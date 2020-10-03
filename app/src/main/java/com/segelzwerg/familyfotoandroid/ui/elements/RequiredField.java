08cfae9d8865c71b54515099a8f6ab3ecf8bd9f4
Segelzwerg <marcel.haas@hhu.de>
2020-10-03 18:10:59 +0200

    ADD: java doc

--- app/src/main/java/com/segelzwerg/familyfotoandroid/ui/elements/RequiredField.java
+++ app/src/main/java/com/segelzwerg/familyfotoandroid/ui/elements/RequiredField.java
@@ -9,15 +9,34 @@ import androidx.appcompat.widget.AppCompatEditText;
 
 import java.util.Objects;
 
+/**
+ * Field of a form that is required before a request can be sent.
+ */
 public class RequiredField extends AppCompatEditText {
+    /**
+     * Constructor.
+     * @param context where the field is initialized
+     */
     public RequiredField(@NonNull Context context) {
         super(context);
     }
 
+    /**
+     * Constructor.
+     * @param context where the field is initialized
+     * @param attrs of the field
+     */
     public RequiredField(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
 
+
+    /**
+     * Constructor.
+     * @param context where the field is initialized
+     * @param attrs of the field
+     * @param defStyleAttr style of the field
+     */
     public RequiredField(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
