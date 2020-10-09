531cefcfeab4baed7fb78cd3efe595ca3e532b49
Segelzwerg <marcel.haas@hhu.de>
2020-10-09 17:13:13 +0200

    UPDATE: rewrite example test to context test

--- app/src/androidTest/java/com/segelzwerg/familyfotoandroid/ContextTest.java
+++ app/src/androidTest/java/com/segelzwerg/familyfotoandroid/ContextTest.java
@@ -3,20 +3,20 @@ package com.segelzwerg.familyfotoandroid;
 import android.content.Context;
 
 import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class ContextTest {
+    /**
+     * Use app context.
+     */
     @Test
     public void useAppContext() {
         // Context of the app under test.
