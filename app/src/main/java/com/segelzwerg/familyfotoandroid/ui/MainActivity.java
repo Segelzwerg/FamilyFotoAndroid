49efdf06bfc196f9137f952562b2d216b6839c73
Segelzwerg <marcel.haas@hhu.de>
2020-10-18 13:33:01 +0200

    ADD: auth token getter for current user

--- app/src/main/java/com/segelzwerg/familyfotoandroid/ui/MainActivity.java
+++ app/src/main/java/com/segelzwerg/familyfotoandroid/ui/MainActivity.java
@@ -1,5 +1,6 @@
 package com.segelzwerg.familyfotoandroid.ui;
 
+import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -7,6 +8,7 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.segelzwerg.familyfotoandroid.familyfotoservice.AuthToken;
 import com.segelzwerg.familyfotoandroid.familyfotoservice.LoginCredentials;
 import com.segelzwerg.familyfotoandroid.familyfotoservice.UserManager;
 
@@ -14,6 +16,16 @@ import com.segelzwerg.familyfotoandroid.familyfotoservice.UserManager;
  * The main activity of this app.
  */
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Manages account provides by Google API.
+     */
+    private UserManager userManager;
+    /**
+     * Current user account.
+     */
+    private Account account;
+
     /**
      * {@inheritDoc}
      */
@@ -24,11 +36,20 @@ public class MainActivity extends AppCompatActivity {
         TextView welcomeTextView = new TextView(this);
 
         AccountManager accountManager = AccountManager.get(this);
-        UserManager userManager = new UserManager(accountManager);
-        userManager.saveAccount(new LoginCredentials("marcel", "1234"));
+        userManager = new UserManager(accountManager);
+        account = userManager.saveAccount(new LoginCredentials("marcel", "1234"));
 
         String welcomeText = "Welcome to Family Foto!";
         welcomeTextView.setText(welcomeText);
         setContentView(welcomeTextView);
+    }
+
+    /**
+     * Gets the auth token for the current user.
+     * @return {@link AuthToken}
+     * @throws Exception collections of things can go wrong.
+     */
+    public AuthToken getAuthToken() throws Exception {
+        return userManager.getAuthToken(account);
     }
 }
