69dff2479d0fe6b5bba7040a8b200c2540c91c2b
Segelzwerg <marcel.haas@hhu.de>
2020-10-20 15:03:43 +0200

    ADD: image loader

---
+++ app/src/main/java/com/segelzwerg/familyfotoandroid/imageService/utils/ImageLoader.java
@@ -0,0 +1,32 @@
+package com.segelzwerg.familyfotoandroid.imageService.utils;
+
+import android.util.Log;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+
+/**
+ * Loads images from the internal storage.
+ */
+public class ImageLoader {
+    /**
+     * Loads the images and returns them as list.
+     * @param path from where the images should be loaded.
+     * @throws IOException is thrown if some images could not be read.
+     * @return {@link List} images as {@link File} in a list.
+     */
+    public static List<File> loadImages(String path) throws IOException {
+        File imagesDirectory = new File(path);
+        if (!imagesDirectory.isDirectory()) {
+            String message = String.format("Directory not found %s", imagesDirectory.toString());
+            Log.e("IO", message);
+            throw new IOException(message);
+        }
+        File[] files = Objects.requireNonNull(imagesDirectory.listFiles());
+        return Arrays.asList(files);
+    }
+}
