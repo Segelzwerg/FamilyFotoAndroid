9bd3275d01899a324b431424f64e02806e531e6c
Segelzwerg <marcel.haas@hhu.de>
2020-10-21 16:26:19 +0200

    UPDATE: move tests and exceptions tests

--- app/src/test/java/com/segelzwerg/familyfotoandroid/imageservice/utils/ImageLoaderUtilTest.java
+++ app/src/test/java/com/segelzwerg/familyfotoandroid/imageservice/utils/ImageLoaderUtilTest.java
@@ -1,4 +1,4 @@
-package com.segelzwerg.familyfotoandroid.imageservice;
+package com.segelzwerg.familyfotoandroid.imageservice.utils;
 
 import org.junit.jupiter.api.Test;
 
@@ -8,9 +8,10 @@ import java.util.List;
 
 import static com.segelzwerg.familyfotoandroid.imageservice.utils.ImageLoaderUtil.loadImages;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ImageLoaderUtilTest {
-
     @Test
     public void loadImagesTest() throws IOException {
         File directory = new File("src/test/resources/");
@@ -18,5 +19,17 @@ public class ImageLoaderUtilTest {
         File testImage = new File("src/test/resources/" + "test-image.jpg");
         assertThat(files).hasSize(1);
         assertThat(files.get(0)).hasSameBinaryContentAs(testImage);
+    }
+
+    @Test
+    public void testPathIsNotDirectory() {
+        assertThatExceptionOfType(IOException.class)
+                .isThrownBy(() -> ImageLoaderUtil.loadImages("text.txt"));
+    }
+
+    @Test
+    public void testPathIsEmpty() {
+        assertThatExceptionOfType(IOException.class)
+                .isThrownBy(() -> ImageLoaderUtil.loadImages("src/test/resources/EmptyDir"));
     }
 }
