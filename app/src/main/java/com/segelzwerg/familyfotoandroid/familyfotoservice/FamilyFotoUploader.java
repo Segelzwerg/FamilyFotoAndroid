3cea28e709ad254cddfea1f75de5a64a7a8e507c
Segelzwerg <marcel.haas@hhu.de>
2020-12-21 17:45:10 +0100

    UPDATE: change return value of upload
    Signed-off-by: Segelzwerg <marcel.haas@hhu.de>

---
+++ app/src/main/java/com/segelzwerg/familyfotoandroid/familyfotoservice/FamilyFotoUploader.java
@@ -0,0 +1,13 @@
+package com.segelzwerg.familyfotoandroid.familyfotoservice;
+
+public class FamilyFotoUploader implements Uploader {
+    /**
+     * Uploads the file to a specific server.
+     *
+     * @param path to the file
+     */
+    @Override
+    public boolean upload(String path) {
+        return true;
+    }
+}
