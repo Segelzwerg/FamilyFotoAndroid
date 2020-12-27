0b81c3ea9abbd9d14e44df90ffe720f0f7988b73
Segelzwerg <marcel.haas@hhu.de>
2020-12-28 00:46:37 +0100

    REMOVE: useless test for async tasks
    Signed-off-by: Segelzwerg <marcel.haas@hhu.de>

--- app/src/test/java/com/segelzwerg/familyfotoandroid/familyfotoservice/FamilyFotoUploaderTest.java
+++ app/src/test/java/com/segelzwerg/familyfotoandroid/familyfotoservice/FamilyFotoUploaderTest.java
@@ -1,15 +1,12 @@
 package com.segelzwerg.familyfotoandroid.familyfotoservice;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
 import okhttp3.RequestBody;
 import retrofit2.Call;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
@@ -40,13 +37,4 @@ public class FamilyFotoUploaderTest {
 
     }
 
-    @Test
-    public void testSuccessfulUpload() {
-        assertTrue(uploader.upload(PATH, successHeader));
-    }
-
-    @Test
-    public void testFailedUpload() {
-        assertFalse(uploader.upload(PATH, failedHeader));
-    }
 }
