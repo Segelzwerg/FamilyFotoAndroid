88d840e0de7fd4e15702eddd10a2de69fe4ee6bc
Segelzwerg <marcel.haas@hhu.de>
2020-12-27 15:17:43 +0100

    ADD: explicit scope
    Signed-off-by: Segelzwerg <marcel.haas@hhu.de>

--- app/src/main/java/com/segelzwerg/familyfotoandroid/imageservice/utils/FileLoaderUtil.java
+++ app/src/main/java/com/segelzwerg/familyfotoandroid/imageservice/utils/FileLoaderUtil.java
@@ -14,7 +14,7 @@ public class FileLoaderUtil {
      * @param filter    loads only files that match the criteria
      * @return a array of {@link File}
      */
-    static File[] getFiles(File directory, FilenameFilter filter) {
+    public static File[] getFiles(File directory, FilenameFilter filter) {
         return directory.listFiles(filter);
     }
 }
