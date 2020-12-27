d1e070844086eeb7113aad9aedd668cf6eda641f
Segelzwerg <marcel.haas@hhu.de>
2020-12-27 16:09:53 +0100

    ADD: upload route
    Signed-off-by: Segelzwerg <marcel.haas@hhu.de>

--- app/src/main/java/com/segelzwerg/familyfotoandroid/familyfotoservice/FamilyFotoServerService.java
+++ app/src/main/java/com/segelzwerg/familyfotoandroid/familyfotoservice/FamilyFotoServerService.java
@@ -2,9 +2,12 @@ package com.segelzwerg.familyfotoandroid.familyfotoservice;
 
 import java.util.Map;
 
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.http.HeaderMap;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 
 /**
  * Offers request to the FamilyFoto server.
@@ -18,4 +21,16 @@ public interface FamilyFotoServerService {
      */
     @POST("/api/token")
     Call<AuthToken> login(@HeaderMap Map<String, String> headers);
+
+    /**
+     * Uploads a photo to the server.
+     *
+     * @param headers contains key value pairs of header arguments
+     * @param file    to be uploaded
+     * @return {@link Response}
+     */
+    @Multipart
+    @POST("/api/upload")
+    Call<Response> upload(@HeaderMap Map<String, String> headers,
+                          @Part("files") RequestBody file);
 }
