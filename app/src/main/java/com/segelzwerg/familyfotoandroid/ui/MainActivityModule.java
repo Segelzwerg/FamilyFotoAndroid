8beee499696b30ed213746ed2acf7481ae94e545
Segelzwerg <marcel.haas@hhu.de>
2020-12-27 16:17:33 +0100

    ADD: server to uploader in provider
    Signed-off-by: Segelzwerg <marcel.haas@hhu.de>

--- app/src/main/java/com/segelzwerg/familyfotoandroid/ui/MainActivityModule.java
+++ app/src/main/java/com/segelzwerg/familyfotoandroid/ui/MainActivityModule.java
@@ -1,5 +1,6 @@
 package com.segelzwerg.familyfotoandroid.ui;
 
+import com.segelzwerg.familyfotoandroid.familyfotoservice.FamilyFotoServerService;
 import com.segelzwerg.familyfotoandroid.familyfotoservice.FamilyFotoUploader;
 import com.segelzwerg.familyfotoandroid.familyfotoservice.Uploader;
 
@@ -17,10 +18,11 @@ public class MainActivityModule {
     /**
      * Provides an uploader to family foto server.
      *
+     * @param server the host to this client
      * @return {@link FamilyFotoUploader}
      */
     @Provides
-    public static Uploader provideUploader() {
-        return new FamilyFotoUploader();
+    public static Uploader provideUploader(FamilyFotoServerService server) {
+        return new FamilyFotoUploader(server);
     }
 }
