040cb0b06e47dbfc523491c9800a0319424fe384
Segelzwerg <marcel.haas@hhu.de>
2020-12-27 16:10:07 +0100

    ADD: upload method
    Signed-off-by: Segelzwerg <marcel.haas@hhu.de>

--- app/src/main/java/com/segelzwerg/familyfotoandroid/familyfotoservice/FamilyFotoUploader.java
+++ app/src/main/java/com/segelzwerg/familyfotoandroid/familyfotoservice/FamilyFotoUploader.java
@@ -1,16 +1,44 @@
 package com.segelzwerg.familyfotoandroid.familyfotoservice;
 
+import android.util.Log;
+
+import com.segelzwerg.familyfotoandroid.imageservice.utils.FileLoaderUtil;
+
+import java.io.File;
+import java.io.IOException;
+
+import lombok.AllArgsConstructor;
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+
 /**
  * Uploads a file to the family foto server.
  */
+@AllArgsConstructor
 public class FamilyFotoUploader implements Uploader {
+    /**
+     * Server where photos are being uploaded.
+     */
+    private final FamilyFotoServerService server;
+
     /**
      * Uploads the file to a specific server.
      *
-     * @param path to the file
+     * @param header contains header arguments
+     * @param path   to the file
      */
     @Override
-    public boolean upload(String path) {
+    public boolean upload(String path, Header header) {
+        File file = FileLoaderUtil.getFile(path);
+        RequestBody requestBody = RequestBody.create(file, MediaType.parse("image/*"));
+        Call<Response> call = server.upload(header.getHeaders(), requestBody);
+        try {
+            call.execute();
+        } catch (IOException e) {
+            Log.e("ERROR", e.getMessage(), e);
+            return false;
+        }
         return true;
     }
 }
