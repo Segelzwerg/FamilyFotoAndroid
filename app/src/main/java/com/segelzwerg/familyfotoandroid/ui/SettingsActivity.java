26d743aa7ee7eb3c91b9d922286b6f47cad03080
Segelzwerg <marcel.haas@hhu.de>
2020-12-28 23:23:13 +0100

    UPDATE: use IntEditText for port input
    Signed-off-by: Segelzwerg <marcel.haas@hhu.de>

--- app/src/main/java/com/segelzwerg/familyfotoandroid/ui/SettingsActivity.java
+++ app/src/main/java/com/segelzwerg/familyfotoandroid/ui/SettingsActivity.java
@@ -1,16 +1,30 @@
 package com.segelzwerg.familyfotoandroid.ui;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.segelzwerg.familyfotoandroid.R;
+import com.segelzwerg.familyfotoandroid.familyfotoservice.BaseUrlModule;
+import com.segelzwerg.familyfotoandroid.ui.elements.IntEditText;
+import com.segelzwerg.familyfotoandroid.ui.elements.StringEditText;
 
 /**
  * UI for dealing with all sorts of settings.
  */
 public class SettingsActivity extends AppCompatActivity {
+
+    /**
+     * Text input for hostname.
+     */
+    private StringEditText editHostname;
+    /**
+     * Text input for port number.
+     */
+    private IntEditText editPort;
+
     /**
      * {@inheritDoc}
      */
@@ -18,5 +32,20 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        editHostname = findViewById(R.id.hostnameInput);
+        editPort = findViewById(R.id.portInput);
+    }
+
+    /**
+     * Saves the settings.
+     *
+     * @param view from where the method is called.
+     */
+    public void saveSettings(View view) {
+        String hostname = editHostname.getString();
+        int port = editPort.getInt();
+        BaseUrlModule.setHost(hostname);
+        BaseUrlModule.setPort(port);
     }
 }
