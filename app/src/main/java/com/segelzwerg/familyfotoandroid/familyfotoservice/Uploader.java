417b9b3ed58a167d8b690714b83cf98813b217e8
Segelzwerg <marcel.haas@hhu.de>
2020-12-28 15:04:11 +0100

    ADD: doc string
    Signed-off-by: Segelzwerg <marcel.haas@hhu.de>

--- app/src/main/java/com/segelzwerg/familyfotoandroid/familyfotoservice/Uploader.java
+++ app/src/main/java/com/segelzwerg/familyfotoandroid/familyfotoservice/Uploader.java
@@ -14,5 +14,11 @@ public interface Uploader {
      */
     void upload(String path, Header header);
 
+    /**
+     * Uploads multiple files.
+     *
+     * @param paths  list of file paths
+     * @param header contains header arguments. E.g. must contain token auth,
+     */
     void uploadFiles(List<String> paths, Header header);
 }
