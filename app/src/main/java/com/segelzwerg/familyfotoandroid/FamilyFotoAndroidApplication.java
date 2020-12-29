0b01f214f6f2aa4dc0d431474f58e1162393692a
Segelzwerg <marcel.haas@hhu.de>
2020-12-29 14:22:51 +0100

    REFACTOR: use constants for keys
    Signed-off-by: Segelzwerg <marcel.haas@hhu.de>

--- app/src/main/java/com/segelzwerg/familyfotoandroid/FamilyFotoAndroidApplication.java
+++ app/src/main/java/com/segelzwerg/familyfotoandroid/FamilyFotoAndroidApplication.java
@@ -19,6 +19,8 @@ public class FamilyFotoAndroidApplication extends Application {
      * Default port used by flask.
      */
     public static final int FLASK_DEFAULT_PORT = 5000;
+    public static final String HOSTNAME_KEY = "hostname";
+    public static final String HOST_PORT_KEY = "host_port";
 
     /**
      * Called when the application is starting, before any activity, service,
@@ -28,8 +30,8 @@ public class FamilyFotoAndroidApplication extends Application {
     public void onCreate() {
         super.onCreate();
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        String hostname = sharedPreferences.getString("hostname", "10.0.2.2");
-        int port = Integer.parseInt(sharedPreferences.getString("host_port",
+        String hostname = sharedPreferences.getString(HOSTNAME_KEY, "10.0.2.2");
+        int port = Integer.parseInt(sharedPreferences.getString(HOST_PORT_KEY,
                 String.valueOf(FLASK_DEFAULT_PORT)));
         BaseUrlModule.setHost(hostname);
         BaseUrlModule.setPort(port);
