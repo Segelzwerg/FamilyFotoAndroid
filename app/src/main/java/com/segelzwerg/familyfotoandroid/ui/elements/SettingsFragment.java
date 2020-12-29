2768519b7c860e0054a5b2110dfdd84f6b8afd9a
Segelzwerg <marcel.haas@hhu.de>
2020-12-29 15:06:48 +0100

    FIX: import for javadoc
    Signed-off-by: Segelzwerg <marcel.haas@hhu.de>

--- app/src/main/java/com/segelzwerg/familyfotoandroid/ui/elements/SettingsFragment.java
+++ app/src/main/java/com/segelzwerg/familyfotoandroid/ui/elements/SettingsFragment.java
@@ -3,7 +3,6 @@ package com.segelzwerg.familyfotoandroid.ui.elements;
 import android.os.Bundle;
 
 import androidx.preference.PreferenceFragmentCompat;
-import androidx.preference.PreferenceScreen;
 
 import com.segelzwerg.familyfotoandroid.R;
 
@@ -13,13 +12,11 @@ import com.segelzwerg.familyfotoandroid.R;
 public class SettingsFragment extends PreferenceFragmentCompat {
     /**
      * Called during {@link #onCreate(Bundle)} to supply the preferences for this fragment.
-     * Subclasses are expected to call {@link #setPreferenceScreen(PreferenceScreen)} either
-     * directly or via helper methods such as {@link #addPreferencesFromResource(int)}.
      *
      * @param savedInstanceState If the fragment is being re-created from a previous saved state,
      *                           this is the state.
      * @param rootKey            If non-null, this preference fragment should be rooted at the
-     *                           {@link PreferenceScreen} with this key.
+     *                           PreferenceScreen with this key.
      */
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
