958a28f40134a62c70ef6e8f6b407404cbca98ac
Segelzwerg <marcel.haas@hhu.de>
2020-12-29 15:00:06 +0100

    ADD: java doc REFACTOR: constant
    Signed-off-by: Segelzwerg <marcel.haas@hhu.de>

--- app/src/main/java/com/segelzwerg/familyfotoandroid/FamilyFotoAndroidApplication.java
+++ app/src/main/java/com/segelzwerg/familyfotoandroid/FamilyFotoAndroidApplication.java
@@ -19,8 +19,19 @@ public class FamilyFotoAndroidApplication extends Application {
      * Default port used by flask.
      */
     public static final int FLASK_DEFAULT_PORT = 5000;
+    /**
+     * Key for the hostname.
+     */
     public static final String HOSTNAME_KEY = "hostname";
+    /**
+     * Key for the host port.
+     */
     public static final String HOST_PORT_KEY = "host_port";
+    /**
+     * Is the server ip running the app in an emulator.
+     */
+    @SuppressWarnings("PMD.AvoidUsingHardCodedIP")
+    public static final String EMULATOR_HOST = "10.0.2.2";
 
     /**
      * Called when the application is starting, before any activity, service,
@@ -29,8 +40,9 @@ public class FamilyFotoAndroidApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        String hostname = sharedPreferences.getString(HOSTNAME_KEY, "10.0.2.2");
+        SharedPreferences sharedPreferences = PreferenceManager.
+                getDefaultSharedPreferences(this);
+        String hostname = sharedPreferences.getString(HOSTNAME_KEY, EMULATOR_HOST);
         int port = Integer.parseInt(sharedPreferences.getString(HOST_PORT_KEY,
                 String.valueOf(FLASK_DEFAULT_PORT)));
         BaseUrlModule.setHost(hostname);
