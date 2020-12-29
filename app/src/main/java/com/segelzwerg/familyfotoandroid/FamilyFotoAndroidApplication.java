37c1fe061c1c3a783634e23e5951baad57bd346d
Segelzwerg <marcel.haas@hhu.de>
2020-12-29 13:23:10 +0100

    UPDATE: load network settings from preferences
    Signed-off-by: Segelzwerg <marcel.haas@hhu.de>

--- app/src/main/java/com/segelzwerg/familyfotoandroid/FamilyFotoAndroidApplication.java
+++ app/src/main/java/com/segelzwerg/familyfotoandroid/FamilyFotoAndroidApplication.java
@@ -1,6 +1,11 @@
 package com.segelzwerg.familyfotoandroid;
 
 import android.app.Application;
+import android.content.SharedPreferences;
+
+import androidx.preference.PreferenceManager;
+
+import com.segelzwerg.familyfotoandroid.familyfotoservice.BaseUrlModule;
 
 import dagger.hilt.android.HiltAndroidApp;
 
@@ -9,4 +14,24 @@ import dagger.hilt.android.HiltAndroidApp;
  */
 @HiltAndroidApp
 public class FamilyFotoAndroidApplication extends Application {
+
+    /**
+     * Default port used by flask.
+     */
+    public static final int FLASK_DEFAULT_PORT = 5000;
+
+    /**
+     * Called when the application is starting, before any activity, service,
+     * or receiver objects (excluding content providers) have been created.
+     */
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String hostname = sharedPreferences.getString("hostname", "10.0.2.2");
+        int port = Integer.parseInt(sharedPreferences.getString("host_port",
+                String.valueOf(FLASK_DEFAULT_PORT)));
+        BaseUrlModule.setHost(hostname);
+        BaseUrlModule.setPort(port);
+    }
 }
