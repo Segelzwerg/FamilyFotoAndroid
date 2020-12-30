352260dc5a88ee29af1f7827c3ec0335c58133d6
Segelzwerg <marcel.haas@hhu.de>
2020-12-30 20:27:22 +0100

    ADD: java doc
    Signed-off-by: Segelzwerg <marcel.haas@hhu.de>

--- app/src/main/java/com/segelzwerg/familyfotoandroid/ui/ActivityBack.java
+++ app/src/main/java/com/segelzwerg/familyfotoandroid/ui/ActivityBack.java
@@ -12,14 +12,24 @@ import com.segelzwerg.familyfotoandroid.ui.utils.ActivityUtils;
  * Abstract class that has a back button in the top action bar.
  */
 public abstract class ActivityBack extends AppCompatActivity {
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityUtils.enableBackButton(getSupportActionBar());
     }
 
+    /**
+     * Goes back to the previous activity unless it is the first one called. Then it goes to the
+     * main activity.
+     *
+     * @param item the menu item being pressed. In this case the back button.
+     * @return if it was successful.
+     */
     @Override
-    public final boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == android.R.id.home) {
             ActivityUtils.gotoActivity(this, MainActivity.class);
